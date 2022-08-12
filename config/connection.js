134b04f66e38469a44dcdcad36b3fd32d1838c74
Moshe <mobgadol@gmail.com>
2022-08-11 23:13:57 -0400

    added feature/frontend

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,10 @@ const Sequelize = require('sequlize');
 
 require('dotenv').config();
 
-const sequlize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.PW, {
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-
 });
 
-module.exports = sequlize;
+module.exports = sequelize;
