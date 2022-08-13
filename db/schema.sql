321e1ba46b7f9168abce4aebf8a398ed35428550
Dawson Tilley <16dit@queenus.ca>
2022-08-13 13:26:52 -0400

    sync main, troubleshot routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 DROP DATABASE IF EXISTS quote_db;
 
 CREATE DATABASE quote_db;
+=======
+DROP DATABASE IF EXISTS inspirational_quote_db;
+
+CREATE DATABASE inspirational_quote_db;
+>>>>>>> d7c26d83b1d4878247b623c51884d9ca2fcf04f0
