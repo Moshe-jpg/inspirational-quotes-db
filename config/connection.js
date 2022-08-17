f38521113746714689ca374b20d906dc2c557ba3
Moshe <mobgadol@gmail.com>
2022-08-16 21:21:29 -0400

    fixed connection issues

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // create connection to our db
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
+let sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
